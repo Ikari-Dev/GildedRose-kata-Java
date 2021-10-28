d37a4762537c07828577c3002321ce0c0b98765d
fldelcroix <florian.delcroix982@gmail.com>
2021-10-28 21:15:42 +0200

    extract decrease sellIn
    and fix error

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,9 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-               
                 decreaseQuality(item);
-                
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
@@ -21,7 +19,7 @@ class GildedRose {
                 }
             }
 
-            decreaseQuality(item);
+            decreaseSellIn(item);
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
@@ -53,13 +51,19 @@ class GildedRose {
         if (item.quality < 50) {
             item.quality++;
         }
-    } 
-    
+    }  
+
     private void decreaseQuality(Item item){
         if (item.quality > 0) {
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.quality = item.quality - 1;
             }
+        }
+    }  
+
+    private void decreaseSellIn(Item item){
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
         }
     } 
 
